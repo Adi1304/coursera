63ccc3fe7fc90f1fbad8635196a29db89b7bdc5c
Adi1304 <71835592+Adi1304@users.noreply.github.com>
2020-10-18 04:03:39 +0530

    Update SpeakHello.js

--- site3/SpeakHello.js
+++ site3/SpeakHello.js
@@ -1,8 +1,9 @@
+(function(window){var helloSpeaker = {};
 
-var helloSpeaker = {};
-(helloSpeaker.speak = function(name) {
-		var speakWord = "Hello";
-  if ( name!= undefined){
-  console.log(speakWord + " " + name);}
-})();
 
+var speakWord = "Hello";
+	helloSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
+})(window);
